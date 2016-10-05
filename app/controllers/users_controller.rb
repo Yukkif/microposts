8ed19b74123ca838659bb85dae57a34f3fd2f85f
Yukki <f.n620g89@gmail.com>
2016-10-05 15:51:07 +0000

    teisyutu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,53 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only:[:edit,:update]
+
+
   def new
     @user = User.new
   end
-  
+
   def show # 追加
-   @user = User.find(params[:id])
+  @user = User.find(params[:id])
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      session[:user_id] = @user.id
+      flash[:info] = "logged in as #{@user.name}"
       redirect_to @user # ここを修正
     else
       render 'new'
     end
   end
 
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+  def update
+    @user = User.find(params[:id])
+   if @user.update(user_params)
+     redirect_to @user
+    else
+      render 'edit'
+   end
   end
-end
+
+ def edit
+   @user = User.find(params[:id])
+ end
+
+
+ private
+
+ def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation, :region, :profile)
+ end
+
+ def correct_user
+  user = User.find(params[:id])
+  if current_user.id != user.id
+  redirect_to root_path
+  else
+  end
+  end
+  
+ end
